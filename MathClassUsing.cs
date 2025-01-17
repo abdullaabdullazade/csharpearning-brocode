55a06b207ff17695607fe842fda4ca87a20b70ab
Abdulla Abdullazade <89068450+abdullaabdullazade@users.noreply.github.com>
2025-01-17 20:06:16 +0400

    1.17.2025.8.05

--- MathClassUsing.cs
+++ MathClassUsing.cs
@@ -1,28 +1,28 @@
 class MathClassUsing
 {
+    /*
+        static void Main(string[] args)
+        {
+            double x = 4.4, y = 5;
 
-    static void Main(string[] args)
-    {
-        double x = 4.4, y = 5;
+            double a = Math.Pow(y, 3);//power
+            double b = Math.Sqrt(x);//square
+            double c = Math.Abs(x);// 4
+            double d = Math.Round(x);//4.4 =4 ; 4.5=5;
+            double e = Math.Ceiling(x); //4.4 = 5; 4.0 = 4;
+            double f = Math.Floor(x);//4.4=4;5.5=5;-53.12 = -54
+            double g = Math.Max(x, y);
+            double h = Math.Min(x, y);
 
-        double a = Math.Pow(y, 3);//power
-        double b = Math.Sqrt(x);//square
-        double c = Math.Abs(x);// 4
-        double d = Math.Round(x);//4.4 =4 ; 4.5=5;
-        double e = Math.Ceiling(x); //4.4 = 5; 4.0 = 4;
-        double f = Math.Floor(x);//4.4=4;5.5=5;-53.12 = -54
-        double g = Math.Max(x, y);
-        double h = Math.Min(x, y);
-
-        Console.WriteLine();
-        Console.WriteLine();
-        Console.WriteLine(a);
-        Console.WriteLine(b);
-        Console.WriteLine(c);
-        Console.WriteLine(d);
-        Console.WriteLine(e);
-        Console.WriteLine(f);
-        Console.WriteLine(g);
-        Console.WriteLine(h);
-    }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+        }*/
 }
